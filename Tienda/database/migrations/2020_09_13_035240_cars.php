76cb8180a69197018b5d6d13b68579f83f3f7bd4
jframirezr <jframirezr@eafit.edu.co>
2020-09-12 23:16:18 -0500

    Creación de el model y la migración de el carro de compras

---
+++ Tienda/database/migrations/2020_09_13_035240_cars.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Cars extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignId('customer_id');
+            $table->boolean('status');
+            $table->json('item_list');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
