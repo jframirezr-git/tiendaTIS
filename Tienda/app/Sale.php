86f9a8ae6455b99b48e0c8d40fba6fe85a746a5a
jframirezr <jframirezr@eafit.edu.co>
2020-09-12 22:36:53 -0500

    Implementación del modelo de ventas y la base de datos

---
+++ Tienda/app/Sale.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sale extends Model
+{
+     //attributes 
+     protected $fillable = ['unit_value','date', 'invoice', 'quantity', 'item_id', 'customer_id', "car_id"];
+
+     //getters and setters
+     public function getId()
+     {
+         return $this->attributes['id'];
+     }
+ 
+     public function setId($id)
+     {
+         $this->attributes['id'] = $id;
+     }
+ 
+     public function getUniteValue()
+     {
+         return $this->attributes['unit_value'];
+     }
+ 
+     public function setUniteValue($unit_value)
+     {
+         $this->attributes['unit_value'] = $unit_value;
+     }
+ 
+     public function getDate()
+     {
+         return $this->attributes['date'];
+     }
+ 
+     public function setDate($date)
+     {
+         $this->attributes['date'] = $date;
+     }
+     public function getInvoice()
+     {
+         return $this->attributes['invoice'];
+     }
+ 
+     public function setInvoice($invoice)
+     {
+         $this->attributes['invoice'] = $invoice;
+     }
+     public function getQuantity()
+     {
+         return $this->attributes['quantity'];
+     }
+ 
+     public function setQuantity($quantity)
+     {
+         $this->attributes['quantity'] = $quantity;
+     }
+
+     public function getItemId()
+     {
+         return $this->attributes['item_id'];
+     }
+ 
+     public function setItemId($item_id)
+     {
+         $this->attributes['item_id'] = $item_id;
+     }
+
+     public function getCustomerId()
+     {
+         return $this->attributes['customer_id'];
+     }
+ 
+     public function setCustomerId($customer_id)
+     {
+         $this->attributes['customer_id'] = $customer_id;
+     }
+
+     public function getCarId()
+     {
+        $this->attributes['car_id'] = $car_id;
+     }
+
+     public function setCarId($car_id)
+     {
+         $this->attributes['car_id'] = $car_id;
+     }
+
+     //relationships
+     public function customer()
+     {
+        return $this->belongsTo('App\Customer');
+     }
+
+     public function car()
+     {
+        return $this->belongsTo('App\Car');
+     }
+
+}
