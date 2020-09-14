503217dc11959902f04a852f903ac485914e3177
jealzateu <jealzateu@eafit.edu.co>
2020-09-14 18:11:57 -0500

    Input controller

--- Tienda/app/Http/Controllers/InputController.php
+++ Tienda/app/Http/Controllers/InputController.php
@@ -10,4 +10,58 @@ class InputController extends Controller
      return view('input.index');
     }
 
+    public function showInputs(){
+        $data = [];
+
+        $data["title"] = "Clientes registrados";
+        $data["all_input"] = Input::all();
+
+        return view('input.showInputs')->with("data",$data);
+    }
+
+    public function show($id)
+    {
+        $data = [];
+
+        $data_input = Input::findOrFail($id);
+
+        $data["value"] = $data_input->getValue();
+        $data["date"] = $data_input->getDate();
+        $data["bill"] = $data_input->getBill();
+        $data["quantity"] = $data_input->getQuantity();
+        $data["inputs"] = $data_input;
+
+        return view('input.show')->with("data",$data);
+    }
+
+    public function create()
+    {
+        $data = [];
+        $data["title"] = "Crear cliente";
+        $data["all_input"] = Input::all();
+
+        return view('input.create')->with("data",$data);
+    }
+
+    public function save(Request $request)
+    {
+        $request->validate([
+            "value" => "required",
+            "date" => "required",
+            "bill" => "required",
+            "quantity" => "required",
+        ]);
+        Input::create($request->only(["value","date","bill","quantity"]));
+
+        return back()->with('Éxito','Elemento creado satisfactoriamente');
+    }
+
+    public function deleteinput($id)
+    {
+        $data_input = Input::find($id);                
+        $data_input->delete();
+
+        return redirect('input/show');
+    }
+
 }
