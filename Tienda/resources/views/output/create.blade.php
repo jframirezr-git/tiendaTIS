80b508de8e706f9b1df8cd49bd1fdcf1b34f973a
jealzateu <jealzateu@eafit.edu.co>
2020-09-14 19:58:22 -0500

    estilos

--- Tienda/resources/views/output/create.blade.php
+++ Tienda/resources/views/output/create.blade.php
@@ -3,33 +3,35 @@
 @section("title", $data["title"])
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-    <div class="col-md-8">
-    @include('util.message')
-            <div class="card">
-                <div class="card-header">Crear Salida</div>
-                <div class="card-body">
-                @if($errors->any())
-                <ul id="errors">
-                    @foreach($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-                @endif
+<div class="row text-center justify-content-center align-items-center" style="margin: 5px;margin-top: 5%;">
+    <div class="col">
+        <div class="container">
+            <h2>Crear Salida</h2>
+            @include('util.message')
+            @if($errors->any())
+            <ul id="errors">
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+            </ul>
+            @endif
 
-                <form method="POST" action="{{ route('output.save') }}">
-                    @csrf
-                    <input type="number" placeholder="Ingresa el valor" name="value" value="{{ old('value') }}" /><br>
-                    <input type="date" placeholder="Ingresa la fecha" name="date" value="{{ old('date') }}" /><br>
-                    <input type="number" placeholder="Ingresa la cantidad" name="quantity" value="{{ old('quantity') }}" /><br>
-                    <input type="number" placeholder="Ingresa el articulo" name="item_id" value="{{ old('item_id') }}" /><br>
-                    <input type="submit" value="Crear" />
-                </form>
-                <input type="button" onclick="window.location.href='/output'" value="Volver" />
+            <form method="POST" action="{{ route('output.save') }}">
+                @csrf
+                <div class="form-row text-center justify-content-center align-items-center">
+                    <div class="col-6"><label class="col-form-label">Valor:</label></div>
+                    <div class="col-6"><input class="form-control" type="number" placeholder="Ingresa el valor" name="value"></div>
+                    <div class="col-6"><label class="col-form-label">Fecha:</label></div>
+                    <div class="col-6"><input class="form-control" type="date" placeholder="Ingresa la fecha" name="date"></div>
+                    <div class="col-6"><label class="col-form-label">Cantidad:</label></div>
+                    <div class="col"><input class="form-control" type="number" placeholder="Ingresa la cantidad" name="quantity"></div>
+                    <div class="col-6"><label class="col-form-label">Id del articulo:</label></div>
+                    <div class="col"><input class="form-control" type="number" placeholder="Ingresa el articulo id" name="item_id"></div>
                 </div>
-            </div>
+                <input class="btn btn-primary" style="margin: 5px;" type="submit" value="Crear" />
+            </form>
         </div>
     </div>
 </div>
+
 @endsection
--- Tienda/resources/views/output/showOutputs.blade.php
+++ Tienda/resources/views/output/showOutputs.blade.php
@@ -2,26 +2,37 @@
 
 @section("title", $data["title"])
 
+@section('styles')
+  @parent
+    <link href="{{ asset('css/Article-Clean.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/Article-Dual-Column.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/Article-List.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/Navigation-Clean.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/Navigation-with-Search.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/styles.css')}}" rel="stylesheet">
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+<div class="row">
+    @foreach($data["all_input"] as $all_input)
+    <div class="col-4 text-center d-lg-flex align-self-center justify-content-lg-start align-items-lg-end">
+        <div class="container">
             <div class="card">
-                <div class="card-header">Salidas registradas</div>
                 <div class="card-body">
-                <ul id="errors">
-                @foreach($data["all_output"] as $all_output)
-                    <li>{{ $all_output->getId() }}</li>
-                    <li>{{ $all_output->getValue() }}</li>
-                    <li>{{ $all_output->getDate() }}</li>
-                    <li>{{ $all_output->getQuantity() }}</li>
-                    <a class="btn btn-primary" href="{{ route('output.show', ['id' => $all_output->getId()]) }}">Ver más</a></br></br>
-                @endforeach
-                </ul>
-                <input type="button" onclick="window.location.href='/output'" value="Volver" />
+                    <h4 class="card-title">{{$all_output->getId()}}</h4>
+                    <h6 class="text-muted card-subtitle mb-2">Valor:{{$all_output->getValue()}}</h6>
+                    <h6 class="text-muted card-subtitle mb-2">Cantidad:{{$all_output->getQuantity()}}</h6>
+                    <a class="btn btn-primary" href="{{ route('output.show', ['id' => $all_output->getId()]) }}">Ver más</a>
                 </div>
             </div>
         </div>
     </div>
+    @endforeach
 </div>
+<div class="row text-center">
+    <div class="col">
+    <a class="btn btn-primary" type="button" style="margin: 5px;" href="{{ url('/output/create') }}">Crear Salida</a>
+    </div>
+</div>
+
 @endsection
