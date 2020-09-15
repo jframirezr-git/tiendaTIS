7ea203dcc2db4de9d92e80a0e163291ff5f692cb
jealzateu <jealzateu@eafit.edu.co>
2020-09-14 19:44:55 -0500

    estilos

--- Tienda/resources/views/input/showInputs.blade.php
+++ Tienda/resources/views/input/showInputs.blade.php
@@ -2,27 +2,36 @@
 
 @section("title", $data["title"])
 
+@section('styles')
+  @parent
+    <link href="{{ asset('css/Article-Clean.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/Article-Dual-Column.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/Article-List.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/Navigation-Clean.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/Navigation-with-Search.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/styles.css')}}" rel="stylesheet">
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+<div class="row">
+    @foreach($data["all_input"] as $all_input)
+    <div class="col-4 text-center d-lg-flex align-self-center justify-content-lg-start align-items-lg-end">
+        <div class="container">
             <div class="card">
-                <div class="card-header">Entradas registradas</div>
                 <div class="card-body">
-                <ul id="errors">
-                @foreach($data["all_input"] as $all_input)
-                    <li>{{ $all_input->getId() }}</li>
-                    <li>{{ $all_input->getValue() }}</li>
-                    <li>{{ $all_input->getDate() }}</li>
-                    <li>{{ $all_input->getBill() }}</li>
-                    <li>{{ $all_input->getQuantity() }}</li>
-                    <a class="btn btn-primary" href="{{ route('input.show', ['id' => $all_input->getId()]) }}">Ver más</a></br></br>
-                @endforeach
-                </ul>
-                <input type="button" onclick="window.location.href='/input'" value="Volver" />
+                    <h4 class="card-title">{{$all_all_input->getId()}}</h4>
+                    <h6 class="text-muted card-subtitle mb-2">Valor:{{$all_input->getValue()}}</h6>
+                    <h6 class="text-muted card-subtitle mb-2">Cantidad:{{$all_input->getQuantity()}}</h6>
+                    <a class="btn btn-primary" href="{{ route('input.show', ['id' => $all_input->getId()]) }}">Ver más</a>
                 </div>
             </div>
         </div>
+    </div>
+    @endforeach
+</div>
+<div class="row text-center">
+    <div class="col">
+    <a class="btn btn-primary" type="button" style="margin: 5px;" href="{{ url('/input/create') }}">Crear Entrada</a>
     </div>
 </div>
 @endsection
